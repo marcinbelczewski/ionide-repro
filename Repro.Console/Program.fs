3b923e7900440d1f7c1cff14cca1db3a37b8640c
Marcin Belczewski <marcin.belczewski@ihsmarkit.com>
2018-10-22 17:59:23 +0200

    First commit

---
+++ Repro.Console/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Repro.Say
+
+[<EntryPoint>]
+let main argv =
+    hello "Repro"
+    0 // return an integer exit code
---
+++ Repro/Library.fs
@@ -0,0 +1,5 @@
+namespace Repro
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
